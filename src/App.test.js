2edd02bdc6eb2dc31341e9d4bfbaf51dfec096c0
devYooooong <112360946+devYooooong@users.noreply.github.com>
2022-08-30 14:38:38 +0900

    modify test msg

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText("TEST11111");
+  const linkElement = screen.getByText("Hello React_C5^^");
   expect(linkElement).toBeInTheDocument();
 });
